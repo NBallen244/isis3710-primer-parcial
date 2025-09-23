b22b50f87115eeb8d43131771c0b1567bad991ec
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2025-09-23 18:33:40 -0500

    vere

--- src/app/components/home/tarjetaPokemon.tsx
+++ src/app/components/home/tarjetaPokemon.tsx
@@ -29,11 +29,9 @@ const TarjetaPokemon=({pokemon}:{pokemon:Pokemon})=>{
     },[]);
     return (
         <div className="border border-red-500 p-4">
-        <Link hred=`/detail/${id}`>
         <img src={info.sprites.back_default!} alt={pokemon.name}  className="w-48 h-48 mb-2 object-contain flex flex-col justify-items-center"/>
         <h2 className="text-lg font-bold text-black">{pokemon.name}</h2>
         <h3 className="text-lg font-bold rounded-xl text-black bg-green-500">{info.types[0].type.name}</h3>
-        </Link>
         </div>
     );
 }
